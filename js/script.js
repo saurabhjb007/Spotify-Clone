bae7aaaab32914e60e3366c12af86728e98f2f76
saurabhjb007 <saurabhsharma132000@gmail.com>
2024-03-16 20:04:51 +0530

    fetch

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ async function displayAlbums() {
     if (e.href.includes("/songs/")) {
       let folder = e.href.split("/")[4];
       //  get the Metadata of folder
-      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+      let a = await fetch(`/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML =
         cardContainer.innerHTML +
